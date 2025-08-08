250630193685b3dcbfb98b324317a5780c6cd5e7
freemanyoo <freemanyoo@gmail.com>
2025-08-08 15:51:26 +0900

    250808

--- ch05-vite-test/src/App.jsx
+++ ch05-vite-test/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import ScrollBox from "./component/ScrollBox";
+import ValidationSample from "./component/ValidationSample";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+// function App() {
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1 className="react">ch5 리액트 버전으로 ref 확인해보기 </h1>
+        <ValidationSample />
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨밑으로
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button onClick={() => this.ScrollBox.scrollToTop()}>맨위로</button>
+        <button onClick={() => this.ScrollBox.scrollToMiddle()}>가운데</button>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
